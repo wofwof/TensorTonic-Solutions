ec8827a59bda3fd627a4da42b78cf0b6c09b6318
wofwof <krzysztof.pieta@protonmail.com>
2026-09-20 21:39:22 +0200

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 def matrix_transpose(A: list) -> np.ndarray:
-    m = np.array(A)
-    return np.ndarray.transpose(m)
+    a_nd: np.ndarray = np.asarray(A)
+    n, m = a_nd.shape
+    trans = np.zeros((m, n), dtype=a_nd.dtype)
+    for i in range(n):
+        for j in range(m):
+            trans[j, i] = a_nd[i, j]
+    return trans
